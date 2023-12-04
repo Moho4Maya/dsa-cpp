6cf38f70e94f0b43cfbbabf3e8902d277283c099
ashwathama <ashwathama249@gmail.com>
2023-12-04 17:11:51 +0530

    test created

--- test.cpp
+++ test.cpp
@@ -1,10 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void explainpair(){
-    pair<int, int> p = {1,3};
-    cout << p.first << " " << p.second;
-}
+
 
 int main(){
     explainpair();
